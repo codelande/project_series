91fca11237dd56897a4fd725d3eb19ced5420d29
codelande <corentin-delande_student2022@wilder.school>
2022-05-27 09:38:07 +0200

    quete crud

--- src/Entity/Season.php
+++ src/Entity/Season.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Entity\Episode;
 use App\Repository\SeasonRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
